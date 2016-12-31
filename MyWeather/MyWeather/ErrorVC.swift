1fe9b32be7f13634aa5a9cd455268545871a0fc4
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2016-12-31 20:13:20 +0530

    Error VC Created

---
+++ MyWeather/MyWeather/ErrorVC.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorVC.swift
+//  MyWeather
+//
+//  Created by Sandeep Yadav Mattepu on 12/31/16.
+//  Copyright © 2016 Mattepu. All rights reserved.
+//
+
+import UIKit
+
+class ErrorVC: UIViewController
+{
+
+    @IBOutlet weak var reasonForRequest : UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
